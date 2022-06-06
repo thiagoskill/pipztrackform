c203d23971efdac26e6c477dcb235f2605cc73e4
thiagoskill <thiagoskill@gmail.com>
2022-06-06 19:09:28 -0300

    Alterações NO teste para disparo de Automacao EventoTesteSimples - Track Demonstração

--- JS/script.js
+++ JS/script.js
@@ -7,16 +7,36 @@ let idade = document.getElementById("idadePipz");
 
 let emailIdentificar = document.getElementById("emailPipzId");
 
+let mensagens = ['Esta mensagem faz Parte de um Array, no slot 0' , 
+                 'Esta mensagem faz parte de um Array, no slot 1'  ,
+                ];
+let soma = document.getElementById("numerodaSoma");
+
 
 
     function eventos_pipz(){
-      pipz.track("teste", {"email": email.value , "Nome": nome.value , "Idade":idade.value});
-      alert ("apertou o botão")
+      pipz.track("teste", 
+        {"email": email.value , 
+        "Nome": nome.value , 
+        "Idade":idade.value , 
+        "mensagem" : "Esse evento foi lançado como teste"});
     }
 
     function identificar(){
-        pipz.identify( emailIdentificar.value ,{
-            "email" : emailIdentificar.value});
-        console.log (emailIdentificar);
-        console.log (emailIdentificar.value);
+        pipz.identify( emailIdentificar.value ,
+            {"email" : emailIdentificar.value , 
+             "mensagem" : "identificado via Front-End"
+        });
+        alert(" Identificar enviado");
+    }
+
+    function eventSimples() {
+        pipz.track("eventoSimpes",{ 
+            "nome" : "Brewmaster" ,
+            "autor" : "Thiago Alves No JS",
+            "email" : "thiagoskill@gmail.com" , 
+            "mensagem" : mensagens , 
+            "numeroSoma1" : soma.value 
+           })
+        alert("Evento Simples enviado")
     }
